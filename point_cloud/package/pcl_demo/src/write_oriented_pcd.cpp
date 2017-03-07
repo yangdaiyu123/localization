35395c36b8c4a40a80ba7287a8dcfaf1aadfd65f
wlh <wengge987@163.com>
2017-03-07 14:50:52 +0800

    重写了生成PointXYZO地图文件的节点

--- point_cloud/package/pcl_demo/src/write_oriented_pcd.cpp
+++ point_cloud/package/pcl_demo/src/write_oriented_pcd.cpp
@@ -16,68 +16,49 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/filters/passthrough.h>
 
-#include <pcl_ros/point_cloud.h>
-#include <point_xyzo.h>
-
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/point_cloud_conversion.h>
 
 #include <tf/tf.h>
 #include <tf/transform_listener.h>
 #include "tf/transform_datatypes.h"
-#include <sstream>
-
-#include <message_filters/subscriber.h>
-#include <message_filters/time_synchronizer.h>
-#include <message_filters/sync_policies/approximate_time.h>
-
 #include <geometry_msgs/PoseStamped.h>
-#include <visualization_msgs/Marker.h>
+
+#include <pcl_ros/point_cloud.h>
+#include <point_xyzo.h>
+
+#include <sstream>
+#include <string>
 
 using namespace std;
-using namespace message_filters;
 
-pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_in(new pcl::PointCloud<pcl::PointXYZRGB>);
-
-pcl::PointCloud<pcl::PointXYZRGB>::Ptr curb_front(new pcl::PointCloud<pcl::PointXYZRGB>);
-pcl::PointCloud<pcl::PointXYZRGB>::Ptr curb_rear(new pcl::PointCloud<pcl::PointXYZRGB>);
-int cnt = 0;
+pcl::PointCloud<PointXYZO>::Ptr cloud_in(new pcl::PointCloud<PointXYZO>);
+pcl::PointCloud<PointXYZO>::Ptr cloud_out(new pcl::PointCloud<PointXYZO>);
+pcl::PointCloud<PointXYZO>::Ptr cloud_curb(new pcl::PointCloud<PointXYZO>);
+pcl::PointCloud<PointXYZO>::Ptr cloud_sign(new pcl::PointCloud<PointXYZO>);
 
 tf::StampedTransform transform1;
 tf::StampedTransform transform2;
+tf::StampedTransform transform3;
 
 geometry_msgs::PoseStamped pose;
+vector<tf::Vector3> grid_array;
+tf::Vector3 grid(0,0,0);
 
-const int color_red =       0xff0000;
-const int color_orange =    0xff8800;
-const int color_yellow =    0xffff00;
-const int color_green =     0x00ff00;
-const int color_blue =      0x0000ff;
-const int color_violet =    0xff00ff;
+string map_path;
+int grid_scale;
 
-
-//typedef PointXYZIR VPoint;
-//typedef pcl::PointCloud<VPoint> VPointCloud;
-//const VPointCloud::ConstPtr &inMsg
 void frontCurbCloudCallback(const OPointCloud::ConstPtr &cloud_in)
 {
-//	pcl::fromROSMsg(*msg, *cloud_in);
-
 	try
 	{
 		static tf::TransformListener trf_listener1;
         trf_listener1.lookupTransform("map", "base_link", ros::Time(0), transform1);
-        pcl::PointCloud<PointXYZO>::Ptr cloud_out(new pcl::PointCloud<PointXYZO>);
 
         for (int i=0; i<cloud_in->size(); i++)
         {
             tf::Vector3 pt(cloud_in->points[i].x, cloud_in->points[i].y, cloud_in->points[i].z);
             tf::Vector3 converted = transform1 * pt;
-
-			double yaw, pitch, roll;
-			transform1.getBasis().getRPY(roll, pitch, yaw);
-			std::cout<< "in RPY (radian) [" <<  roll << ", " << pitch << ", " << yaw << "]" << std::endl
-			<< "in RPY (degree) [" <<  roll*180.0/M_PI << ", " << pitch*180.0/M_PI << ", " << yaw*180.0/M_PI << "]" << std::endl;
 
             PointXYZO point_in_map;
             point_in_map.x = converted.x();
@@ -85,11 +66,8 @@ void frontCurbCloudCallback(const OPointCloud::ConstPtr &cloud_in)
             point_in_map.z = converted.z();
             point_in_map.orientation = cloud_in->points[i].orientation;
 
-            cloud_out->push_back(point_in_map);
+            cloud_curb->push_back(point_in_map);
         }
-        std::stringstream ss;
-		ss<<cnt++;
-//        pcl::io::savePCDFileASCII ("/home/wlh/oriented_point_map/"+ss.str()+".pcd", *cloud_out);
     }
     catch (tf::TransformException ex) {
         ROS_INFO("%s", ex.what());
@@ -97,52 +75,26 @@ void frontCurbCloudCallback(const OPointCloud::ConstPtr &cloud_in)
     }
 }
 
-void rearCurbCloudCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
+void rearCurbCloudCallback(const OPointCloud::ConstPtr &cloud_in)
 {
-	pcl::fromROSMsg(*msg, *cloud_in);
-//	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_out(new pcl::PointCloud<pcl::PointXYZRGB>);
-
-//	double roll, pitch ,yaw;
-//	tf::Quaternion q;
-//	tf::quaternionMsgToTF(pose.pose.orientation, q);
-//	tf::Matrix3x3(q).getRPY(roll, pitch, yaw);
-
-//    Eigen::Affine3f trans = Eigen::Affine3f::Identity();
-//	trans.translation() << pose.pose.position.x, pose.pose.position.y,pose.pose.position.z;
-//	trans.rotate(Eigen::AngleAxisf(yaw, Eigen::Vector3f::UnitZ()));
-//	pcl::transformPointCloud(*cloud_in, *cloud_out, trans);
-
-
-//	cloud_out->header.frame_id = "map";
-//	sensor_msgs::PointCloud2 cloud_to_pub;
-//    pcl::toROSMsg(*cloud_out, cloud_to_pub);
-//    pub_rear_curb.publish(cloud_to_pub);
-//	std::stringstream ss;
-//	ss<<cnt++;
-//	pcl::io::savePCDFileASCII ("/home/wlh/point_map/"+ss.str()+".pcd", *cloud_out);
-
 	try
 	{
 		static tf::TransformListener trf_listener2;
         trf_listener2.lookupTransform("map", "base_link", ros::Time(0), transform2);
-        pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_out(new pcl::PointCloud<pcl::PointXYZRGB>);
 
         for (int i=0; i<cloud_in->size(); i++)
         {
             tf::Vector3 pt(cloud_in->points[i].x, cloud_in->points[i].y, cloud_in->points[i].z);
             tf::Vector3 converted = transform2 * pt;
 
-            pcl::PointXYZRGB point_in_map;
+            PointXYZO point_in_map;
             point_in_map.x = converted.x();
             point_in_map.y = converted.y();
             point_in_map.z = converted.z();
-            point_in_map.rgb = color_green;
+            point_in_map.orientation = cloud_in->points[i].orientation;
 
-            cloud_out->push_back(point_in_map);
+            cloud_curb->push_back(point_in_map);
         }
-        std::stringstream ss;
-		ss<<cnt++;
-//        pcl::io::savePCDFileASCII ("/home/wlh/point_map/"+ss.str()+".pcd", *cloud_out);
     }
     catch (tf::TransformException ex) {
         ROS_INFO("%s", ex.what());
@@ -150,31 +102,26 @@ void rearCurbCloudCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
     }
 }
 
-void signPointsCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
+void signPointsCallback(const OPointCloud::ConstPtr &cloud_in)
 {
-	pcl::fromROSMsg(*msg, *cloud_in);
 	try
 	{
-		static tf::TransformListener trf_listener2;
-        trf_listener2.lookupTransform("map", "base_link", ros::Time(0), transform2);
-        pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_out(new pcl::PointCloud<pcl::PointXYZRGB>);
+		static tf::TransformListener trf_listener3;
+        trf_listener3.lookupTransform("map", "base_link", ros::Time(0), transform3);
 
         for (int i=0; i<cloud_in->size(); i++)
         {
             tf::Vector3 pt(cloud_in->points[i].x, cloud_in->points[i].y, cloud_in->points[i].z);
-            tf::Vector3 converted = transform2 * pt;
+            tf::Vector3 converted = transform3 * pt;
 
-            pcl::PointXYZRGB point_in_map;
+            PointXYZO point_in_map;
             point_in_map.x = converted.x();
             point_in_map.y = converted.y();
             point_in_map.z = converted.z();
-            point_in_map.rgb = color_yellow;
+            point_in_map.orientation = cloud_in->points[i].orientation;
 
-            cloud_out->push_back(point_in_map);
+            cloud_sign->push_back(point_in_map);
         }
-        std::stringstream ss;
-		ss<<cnt++;
-//        pcl::io::savePCDFileASCII ("/home/wlh/point_map/"+ss.str()+".pcd", *cloud_out);
     }
     catch (tf::TransformException ex) {
         ROS_INFO("%s", ex.what());
@@ -185,16 +132,60 @@ void signPointsCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
 void gpsPoseCallback(geometry_msgs::PoseStamped pose_in)
 {
 	pose = pose_in;
+
+	*cloud_out = *cloud_curb + *cloud_sign;
+
+	cout<<"x: "<<int(pose_in.pose.position.x/grid_scale)<<"\ty: "<<int(pose_in.pose.position.y/50)<<endl;
+
+	int grid_x = int(pose_in.pose.position.x/grid_scale);
+	int grid_y = int(pose_in.pose.position.y/grid_scale);
+
+	if(grid.z() == 0)
+	{
+		grid.setX(grid_x);
+		grid.setY(grid_y);
+		grid.setZ(1);
+	}
+	else
+	{
+		if(grid_x != grid.x() || grid_y != grid.y() )
+		{
+			pcl::PointCloud<PointXYZO>::Ptr cloud_old(new pcl::PointCloud<PointXYZO>);
+			std::stringstream ss;
+			ss<<grid.x()<<"_"<<grid.y()<<".pcd"<<endl;
+			cout<<ss.str();
+
+			if (pcl::io::loadPCDFile<PointXYZO> (map_path + ss.str()+".pcd", *cloud_old) == -1)
+				if(!cloud_out->empty())
+					pcl::io::savePCDFileASCII (map_path + ss.str()+".pcd", *cloud_out);
+			else
+			{
+				*cloud_out += *cloud_old;
+				if(!cloud_out->empty())
+					pcl::io::savePCDFileASCII (map_path + ss.str()+".pcd", *cloud_out);
+			}
+
+			cloud_out->clear();
+			cloud_curb->clear();
+			cloud_sign->clear();
+			grid.setX(grid_x);
+			grid.setY(grid_y);
+		}
+	}
 }
 
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "write_pcd");
+	ros::init(argc, argv, "write_pcd_oriented_grid");
 	ros::NodeHandle nh;
 	ros::Subscriber sub_front_curb = nh.subscribe("front_curb_raw",2,frontCurbCloudCallback);
 	ros::Subscriber sub_rear_curb = nh.subscribe("rear_curb_raw",2,rearCurbCloudCallback);
 	ros::Subscriber sub_sign = nh.subscribe("sign_points",2,signPointsCallback);
-	ros::Subscriber sub_gps_pose = nh.subscribe("gps_pose",2,gpsPoseCallback);
+	ros::Subscriber sub_gps_pose = nh.subscribe("truth_pose",2,gpsPoseCallback);
+
+	ros::NodeHandle pnh("~");
+	pnh.param<std::string>("map_path", map_path, "/home/wlh/map/oriented_point_map/");
+	pnh.param("grid_scale", grid_scale, 50);
 
 	ros::spin();
 	return 0;
--- point_cloud/package/pcl_demo/src/write_oriented_pcd3.cpp
+++ point_cloud/package/pcl_demo/src/write_oriented_pcd3.cpp
@@ -16,6 +16,9 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/filters/passthrough.h>
 
+#include <pcl_ros/point_cloud.h>
+#include <point_xyzo.h>
+
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/point_cloud_conversion.h>
 
@@ -35,19 +38,15 @@ using namespace std;
 using namespace message_filters;
 
 pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_in(new pcl::PointCloud<pcl::PointXYZRGB>);
-pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_out(new pcl::PointCloud<pcl::PointXYZRGB>);
-pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_curb(new pcl::PointCloud<pcl::PointXYZRGB>);
-pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_sign(new pcl::PointCloud<pcl::PointXYZRGB>);
 
+pcl::PointCloud<pcl::PointXYZRGB>::Ptr curb_front(new pcl::PointCloud<pcl::PointXYZRGB>);
+pcl::PointCloud<pcl::PointXYZRGB>::Ptr curb_rear(new pcl::PointCloud<pcl::PointXYZRGB>);
 int cnt = 0;
 
 tf::StampedTransform transform1;
 tf::StampedTransform transform2;
-tf::StampedTransform transform3;
 
 geometry_msgs::PoseStamped pose;
-vector<tf::Vector3> grid_array;
-tf::Vector3 grid(0,0,0);
 
 const int color_red =       0xff0000;
 const int color_orange =    0xff8800;
@@ -57,28 +56,40 @@ const int color_blue =      0x0000ff;
 const int color_violet =    0xff00ff;
 
 
-void frontCurbCloudCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
+//typedef PointXYZIR VPoint;
+//typedef pcl::PointCloud<VPoint> VPointCloud;
+//const VPointCloud::ConstPtr &inMsg
+void frontCurbCloudCallback(const OPointCloud::ConstPtr &cloud_in)
 {
-	pcl::fromROSMsg(*msg, *cloud_in);
+//	pcl::fromROSMsg(*msg, *cloud_in);
 
 	try
 	{
 		static tf::TransformListener trf_listener1;
         trf_listener1.lookupTransform("map", "base_link", ros::Time(0), transform1);
+        pcl::PointCloud<PointXYZO>::Ptr cloud_out(new pcl::PointCloud<PointXYZO>);
 
         for (int i=0; i<cloud_in->size(); i++)
         {
             tf::Vector3 pt(cloud_in->points[i].x, cloud_in->points[i].y, cloud_in->points[i].z);
             tf::Vector3 converted = transform1 * pt;
 
-            pcl::PointXYZRGB point_in_map;
+			double yaw, pitch, roll;
+			transform1.getBasis().getRPY(roll, pitch, yaw);
+			std::cout<< "in RPY (radian) [" <<  roll << ", " << pitch << ", " << yaw << "]" << std::endl
+			<< "in RPY (degree) [" <<  roll*180.0/M_PI << ", " << pitch*180.0/M_PI << ", " << yaw*180.0/M_PI << "]" << std::endl;
+
+            PointXYZO point_in_map;
             point_in_map.x = converted.x();
             point_in_map.y = converted.y();
             point_in_map.z = converted.z();
-            point_in_map.rgb = color_green;
+            point_in_map.orientation = cloud_in->points[i].orientation;
 
-            cloud_curb->push_back(point_in_map);
+            cloud_out->push_back(point_in_map);
         }
+        std::stringstream ss;
+		ss<<cnt++;
+//        pcl::io::savePCDFileASCII ("/home/wlh/oriented_point_map/"+ss.str()+".pcd", *cloud_out);
     }
     catch (tf::TransformException ex) {
         ROS_INFO("%s", ex.what());
@@ -89,11 +100,32 @@ void frontCurbCloudCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
 void rearCurbCloudCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
 {
 	pcl::fromROSMsg(*msg, *cloud_in);
+//	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_out(new pcl::PointCloud<pcl::PointXYZRGB>);
+
+//	double roll, pitch ,yaw;
+//	tf::Quaternion q;
+//	tf::quaternionMsgToTF(pose.pose.orientation, q);
+//	tf::Matrix3x3(q).getRPY(roll, pitch, yaw);
+
+//    Eigen::Affine3f trans = Eigen::Affine3f::Identity();
+//	trans.translation() << pose.pose.position.x, pose.pose.position.y,pose.pose.position.z;
+//	trans.rotate(Eigen::AngleAxisf(yaw, Eigen::Vector3f::UnitZ()));
+//	pcl::transformPointCloud(*cloud_in, *cloud_out, trans);
+
+
+//	cloud_out->header.frame_id = "map";
+//	sensor_msgs::PointCloud2 cloud_to_pub;
+//    pcl::toROSMsg(*cloud_out, cloud_to_pub);
+//    pub_rear_curb.publish(cloud_to_pub);
+//	std::stringstream ss;
+//	ss<<cnt++;
+//	pcl::io::savePCDFileASCII ("/home/wlh/point_map/"+ss.str()+".pcd", *cloud_out);
 
 	try
 	{
 		static tf::TransformListener trf_listener2;
         trf_listener2.lookupTransform("map", "base_link", ros::Time(0), transform2);
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_out(new pcl::PointCloud<pcl::PointXYZRGB>);
 
         for (int i=0; i<cloud_in->size(); i++)
         {
@@ -106,8 +138,11 @@ void rearCurbCloudCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
             point_in_map.z = converted.z();
             point_in_map.rgb = color_green;
 
-            cloud_curb->push_back(point_in_map);
+            cloud_out->push_back(point_in_map);
         }
+        std::stringstream ss;
+		ss<<cnt++;
+//        pcl::io::savePCDFileASCII ("/home/wlh/point_map/"+ss.str()+".pcd", *cloud_out);
     }
     catch (tf::TransformException ex) {
         ROS_INFO("%s", ex.what());
@@ -120,13 +155,14 @@ void signPointsCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
 	pcl::fromROSMsg(*msg, *cloud_in);
 	try
 	{
-		static tf::TransformListener trf_listener3;
-        trf_listener3.lookupTransform("map", "base_link", ros::Time(0), transform3);
+		static tf::TransformListener trf_listener2;
+        trf_listener2.lookupTransform("map", "base_link", ros::Time(0), transform2);
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_out(new pcl::PointCloud<pcl::PointXYZRGB>);
 
         for (int i=0; i<cloud_in->size(); i++)
         {
             tf::Vector3 pt(cloud_in->points[i].x, cloud_in->points[i].y, cloud_in->points[i].z);
-            tf::Vector3 converted = transform3 * pt;
+            tf::Vector3 converted = transform2 * pt;
 
             pcl::PointXYZRGB point_in_map;
             point_in_map.x = converted.x();
@@ -134,8 +170,11 @@ void signPointsCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
             point_in_map.z = converted.z();
             point_in_map.rgb = color_yellow;
 
-            cloud_sign->push_back(point_in_map);
+            cloud_out->push_back(point_in_map);
         }
+        std::stringstream ss;
+		ss<<cnt++;
+//        pcl::io::savePCDFileASCII ("/home/wlh/point_map/"+ss.str()+".pcd", *cloud_out);
     }
     catch (tf::TransformException ex) {
         ROS_INFO("%s", ex.what());
@@ -146,47 +185,6 @@ void signPointsCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
 void gpsPoseCallback(geometry_msgs::PoseStamped pose_in)
 {
 	pose = pose_in;
-
-	*cloud_out = *cloud_curb + *cloud_sign;
-
-	cout<<"x: "<<int(pose_in.pose.position.x/50)<<"\ty: "<<int(pose_in.pose.position.y/50)<<endl;
-
-
-	int grid_x = int(pose_in.pose.position.x/50);
-	int grid_y = int(pose_in.pose.position.y/50);
-
-
-
-	if(grid.z() == 0)
-	{
-		grid.setX(grid_x);
-		grid.setY(grid_y);
-		grid.setZ(1);
-	}
-	else
-	{
-		if(grid_x != grid.x() || grid_y != grid.y() )
-		{
-			pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_old(new pcl::PointCloud<pcl::PointXYZRGB>);
-			std::stringstream ss;
-			ss<<grid.x()<<"_"<<grid.y()<<".pcd"<<endl;
-			cout<<ss.str();
-
-			if (pcl::io::loadPCDFile<pcl::PointXYZRGB> ("/home/wlh/point_map/"+ss.str()+".pcd", *cloud_old) == -1)
-				if(!cloud_out->empty())
-					pcl::io::savePCDFileASCII ("/home/wlh/point_map/"+ss.str()+".pcd", *cloud_out);
-			else
-			{
-				*cloud_out += *cloud_old;
-				if(!cloud_out->empty())
-					pcl::io::savePCDFileASCII ("/home/wlh/point_map/"+ss.str()+".pcd", *cloud_out);
-			}
-
-			cloud_out->clear();
-			grid.setX(grid_x);
-			grid.setY(grid_y);
-		}
-	}
 }
 
 int main(int argc, char **argv)
