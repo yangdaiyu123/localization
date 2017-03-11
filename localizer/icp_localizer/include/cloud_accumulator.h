96dbb05ed2b5cbe78f5cc1cec499cf9066644985
wlh-pc <wengge987@163.com>
2017-03-11 22:39:11 +0800

    点云FIFO控制器，包括初始化

--- localizer/icp_localizer/include/cloud_accumulator.h
+++ localizer/icp_localizer/include/cloud_accumulator.h
@@ -29,24 +29,57 @@
 #include <tf/transform_listener.h>
 #include "tf/transform_datatypes.h"
 
-template <typename PointT>
+#include <pcl_ros/point_cloud.h>
+#include "point_xyzo.h"
+
+#include <vector>
+#include <sstream>
+
 class Cloudaccumulator
 {
 public:
 	Cloudaccumulator();
+	void pulseCallback(const std_msgs::Int8MultiArray::ConstPtr& input);
+	void rtkPoseCallback(const geometry_msgs::PoseStamped::ConstPtr& input);
+	void estPoseCallback(const geometry_msgs::PoseStamped::ConstPtr& input);
+	
+	void frontCurbCallback(const OPointCloud::ConstPtr &input);
+	void rearCurbCallback(const OPointCloud::ConstPtr &input);
+	void signCallback(const OPointCloud::ConstPtr &input);
 
 private:
-	pcl::PointCloud<PointT> cloud_new;
-	pcl::PointCloud<PointT> cloud_in_map;
+	pcl::PointCloud<PointXYZO> cloud_new;
+	pcl::PointCloud<PointXYZO> cloud_in_map;
 	int queue_size;
-	geometry_msgs::PoseStamped pose;
+	geometry_msgs::PoseStamped rtk_pose;
+	geometry_msgs::PoseStamped est_pose;
+	
+	ros::NodeHandle nh;
+	
+	ros::Subscriber sub_rtk_pose;
+	ros::Subscriber sub_est_pose;
+	bool use_rtk;
+	
+	ros::Subscriber sub_pulse;
+	ros::Subscriber sub_imu;
+	
+	ros::Subscriber sub_front_curb;
+	ros::Subscriber sub_rear_curb;
+	ros::Subscriber sub_sign;
+
+	ros::Publisher pub_cloud_sum;
+
+
+	Eigen::Affine3f b_to_m;
+	pcl::PointCloud<PointXYZO>::Ptr cloud_sum;
+	float odom_sum;
+		
+
+
+	std::vector<int> fifo_size;
+	tf::StampedTransform trans_front;
 };
 
-template <typename PointT>
-Cloudaccumulator<PointT>::Cloudaccumulator()
-{
-//	queue_size
-}
 
 
 
--- localizer/icp_localizer/src/cloud_accumulator.cpp
+++ localizer/icp_localizer/src/cloud_accumulator.cpp
@@ -1,1 +1,145 @@
 #include "cloud_accumulator.h"
+
+using namespace std;
+
+const double ODOMETRY_FACTOR = 0.0210386;
+
+Cloudaccumulator::Cloudaccumulator()
+{
+	sub_pulse = nh.subscribe("/pulse", 2, &Cloudaccumulator::pulseCallback, this);
+	sub_rtk_pose = nh.subscribe("/rtk_pose", 2, &Cloudaccumulator::rtkPoseCallback, this);
+	sub_est_pose = nh.subscribe("/estimate_pose", 2, &Cloudaccumulator::estPoseCallback, this);
+
+	sub_front_curb = nh.subscribe("front_curb_raw",2, &Cloudaccumulator::frontCurbCallback, this);
+	sub_rear_curb = nh.subscribe("rear_curb_raw",2, &Cloudaccumulator::rearCurbCallback, this);
+	sub_sign = nh.subscribe("sign_points",2, &Cloudaccumulator::signCallback, this);
+
+	pub_cloud_sum = nh.advertise<sensor_msgs::PointCloud2>("feature_points_sum", 2);
+
+	cloud_sum = pcl::PointCloud<PointXYZO>::Ptr(new pcl::PointCloud<PointXYZO>);
+	
+	use_rtk = true;
+	odom_sum = 0.0;
+
+	ros::MultiThreadedSpinner spinner(4);
+	spinner.spin();
+}
+
+void Cloudaccumulator::frontCurbCallback(const OPointCloud::ConstPtr &input)
+{
+	if(use_rtk)
+		cout<<"rtk\n"<<b_to_m.matrix()<<endl;	
+	else
+		cout<<"est\n"<<b_to_m.matrix()<<endl;	
+		
+	PointXYZO po;
+	cloud_new.clear();
+	for(int i=0; i<input->points.size(); i++)
+	{
+		po.x = input->points[i].x;
+		po.y = input->points[i].y;
+		po.z = input->points[i].z;
+		po.orientation = input->points[i].orientation;
+		cloud_new.push_back(po);
+	}
+	pcl::transformPointCloud(cloud_new, cloud_new, b_to_m);
+	cloud_in_map += cloud_new;
+	fifo_size.push_back(cloud_new.size());
+	
+	if (fifo_size.size()>100)
+	{
+		cloud_in_map.erase(cloud_in_map.begin(), cloud_in_map.begin() + fifo_size[0]);
+		fifo_size.erase(fifo_size.begin());
+	}
+	
+	cloud_in_map.header.frame_id = "map";
+	sensor_msgs::PointCloud2 cloud_to_pub;
+	pcl::toROSMsg(cloud_in_map, cloud_to_pub);
+	pub_cloud_sum.publish(cloud_to_pub);
+
+	
+	
+//	for(int i=0; i<input->points.size(); i++)
+//		cout<<"i: "<<i<<"x: "<<input->points[i].x<<" "<<"y: "<<input->points[i].y<<" "<<"orientation: "<<input->points[i].orientation<<endl;
+
+}
+
+void Cloudaccumulator::rearCurbCallback(const OPointCloud::ConstPtr &input)
+{
+
+
+
+}
+
+void Cloudaccumulator::signCallback(const OPointCloud::ConstPtr &input)
+{
+
+
+}
+
+void Cloudaccumulator::rtkPoseCallback(const geometry_msgs::PoseStamped::ConstPtr& input)
+{
+	double roll, pitch, yaw;
+	tf::Quaternion q;
+    tf::quaternionMsgToTF(input->pose.orientation, q);
+    tf::Matrix3x3(q).getRPY(roll, pitch, yaw);
+
+    double x = input->pose.position.x;
+    double y = input->pose.position.y;
+    double z = input->pose.position.z;
+
+    Eigen::Affine3f trans = Eigen::Affine3f::Identity();
+	trans.translation() << x, y, z;
+	trans.rotate(Eigen::AngleAxisf(yaw, Eigen::Vector3f::UnitZ()));
+
+	if(use_rtk)	b_to_m = trans;
+	
+//	cout<<"rtk\t"<<"x: "<<x<<" "<<"y: "<<y<<" "<<"z: "<<z<<" "<<"yaw: "<<yaw<<endl;
+}
+
+void Cloudaccumulator::estPoseCallback(const geometry_msgs::PoseStamped::ConstPtr& input)
+{
+	double roll, pitch, yaw;
+	tf::Quaternion q;
+    tf::quaternionMsgToTF(input->pose.orientation, q);
+    tf::Matrix3x3(q).getRPY(roll, pitch, yaw);
+
+    double x = input->pose.position.x;
+    double y = input->pose.position.y;
+    double z = input->pose.position.z;
+
+    Eigen::Affine3f trans = Eigen::Affine3f::Identity();
+	trans.translation() << x, y, z;
+	trans.rotate(Eigen::AngleAxisf(yaw, Eigen::Vector3f::UnitZ()));
+	
+	if(!use_rtk)	b_to_m = trans;
+	
+//	cout<<"est\t"<<"x: "<<x<<" "<<"y: "<<y<<" "<<"z: "<<z<<" "<<"yaw: "<<yaw<<endl;
+}
+
+void Cloudaccumulator::pulseCallback(const std_msgs::Int8MultiArray::ConstPtr& input)
+{
+	int pulse_inc = (int)input->data[0] + (int)input->data[0];
+	float odom_inc = pulse_inc * ODOMETRY_FACTOR / 2.0;
+	if(use_rtk==true)
+		odom_sum += odom_inc;
+	
+	if(odom_sum > 60)
+	{
+		use_rtk = false;
+		odom_sum = 0;
+	}
+		
+		
+//	cout<<"odom_sum: "<<odom_sum<<endl;
+}
+
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "Cloudaccumulator");
+
+	Cloudaccumulator localizer;
+
+	return 0;
+}
