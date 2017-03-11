7d5452e33fb79a0c4c3ced5101a841a3a1efb973
wlh-pc <wengge987@163.com>
2017-03-11 13:40:18 +0800

    整理启动文件，区分输出输入话题名称

--- data_convert/fix2tfpose/src/fix2tfpose.cpp
+++ data_convert/fix2tfpose/src/fix2tfpose.cpp
@@ -15,21 +15,17 @@ using namespace std;
 
 static ros::Publisher pose_publisher;
 
-static ros::Publisher stat_publisher;
-
 static double yaw;
 static double yaw_filtered;
 
-string topic_pose, topic_gps, topic_yaw;
-string frame_id;
+string pub_topic_pose, sub_topic_gps, sub_topic_yaw;
+string pose_frame_id;
 double origin_lat, origin_lon;
 
 
 void gpsFilteredCallback(const sensor_msgs::NavSatFixConstPtr &msg)
 {
 	geo_pos_conv geo;
-
-
 
     geo.set_plane(origin_lat*M_PI/180, origin_lon*M_PI/180);
     geo.llh_to_xyz(msg->latitude, msg->longitude, msg->altitude);
@@ -41,7 +37,7 @@ void gpsFilteredCallback(const sensor_msgs::NavSatFixConstPtr &msg)
     geometry_msgs::PoseStamped pose;
     pose.header = msg->header;
     pose.header.stamp = msg->header.stamp;
-    pose.header.frame_id = "map";
+    pose.header.frame_id = pose_frame_id;
     pose.pose.position.x = geo.y();
     pose.pose.position.y = geo.x();
     pose.pose.position.z = 0;
@@ -55,7 +51,7 @@ void gpsFilteredCallback(const sensor_msgs::NavSatFixConstPtr &msg)
     transform_filtered.setOrigin(tf::Vector3(pose.pose.position.x, pose.pose.position.y,pose.pose.position.z));
     q_filtered.setRPY(0, 0, yaw_filtered);
     transform_filtered.setRotation(q_filtered);
-    br_filtered.sendTransform( tf::StampedTransform(transform_filtered, msg->header.stamp, "map" ,frame_id));
+    br_filtered.sendTransform( tf::StampedTransform(transform_filtered, msg->header.stamp, "map" ,pose_frame_id));
 }
 
 void yawFilteredCallback(const std_msgs::Float64::ConstPtr& yaw_rad_in)
@@ -69,17 +65,16 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
 
     ros::NodeHandle pnh("~");
-    pnh.param<std::string>("topic_pose", topic_pose, "gps_pose");
-    pnh.param<std::string>("topic_gps", topic_gps, "gps_filtered");
-    pnh.param<std::string>("topic_yaw", topic_yaw, "yaw_filtered");
-    pnh.param<std::string>("frame_id", frame_id, "map");
+    pnh.param<std::string>("pub_topic_pose", pub_topic_pose, "gps_pose");
+    pnh.param<std::string>("sub_topic_gps", sub_topic_gps, "gps_filtered");
+    pnh.param<std::string>("sub_topic_yaw", sub_topic_yaw, "yaw_filtered");
+    pnh.param<std::string>("pose_frame_id", pose_frame_id, "pose_frame");
     pnh.param("origin_lat", origin_lat, 31.0);
     pnh.param("origin_lon", origin_lon, 121.0);
 
-    pose_publisher = nh.advertise<geometry_msgs::PoseStamped>(topic_pose,10);
-    stat_publisher = nh.advertise<std_msgs::Bool>("/gps_stat", 10);
-    ros::Subscriber sub_rtk_filtered = nh.subscribe(topic_gps, 10, gpsFilteredCallback);
-    ros::Subscriber sub_yaw_filtered = nh.subscribe(topic_yaw, 10, yawFilteredCallback);
+    pose_publisher = nh.advertise<geometry_msgs::PoseStamped>(pub_topic_pose,10);
+    ros::Subscriber sub_rtk_filtered = nh.subscribe(sub_topic_gps, 10, gpsFilteredCallback);
+    ros::Subscriber sub_yaw_filtered = nh.subscribe(sub_topic_yaw, 10, yawFilteredCallback);
 
     ros::spin();
     return 0;
